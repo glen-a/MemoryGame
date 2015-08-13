b4ce592b1f65fd7ecdaa9fd8cdfd13e793e81ac0
Glen Wiltshire <gaw844@uowmail.edu.au>
2015-08-13 19:21:42 +1000

    base

---
+++ MemoryGame/app/src/main/java/uow/edu/au/memorygame/TileData.java
@@ -0,0 +1,22 @@
+package uow.edu.au.memorygame;
+
+/**
+ * Created by glen on 13/08/15.
+ */
+public class TileData {
+
+    private int id;
+
+    TileData(){
+
+    }
+
+    int getID(){
+        return id;
+    }
+
+    void setID(int id){
+        this.id = id;
+    }
+
+}
