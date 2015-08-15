ac6978eda4b427b28a58bee80a0c310c7fd72b45
Glen Wiltshire <gaw844@uowmail.edu.au>
2015-08-15 18:17:44 +1000

    part 2 done, check class tile layout thing with ben

---
+++ MemoryGame/app/src/main/java/uow/edu/au/memorygame/TileView.java
@@ -0,0 +1,14 @@
+package uow.edu.au.memorygame;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by glen on 15/08/15.
+ */
+public class TileView extends LinearLayout {
+    public TileView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
